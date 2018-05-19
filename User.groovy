636bead6c3db8d21fc58bbde1daeac79d955817e
ajay <ajay.singh@tothenew.com>
2018-05-19 15:57:22 +0530

    2

---
+++ User.groovy
@@ -0,0 +1,25 @@
+package u
+
+class User {
+
+    String fname
+    String lname
+    String username
+    String email
+    String password
+
+    String bg
+    String obg
+    String sex
+    String haemoglobin
+    String hiv
+    String natTest
+    String hcv
+    String antibodyScreening
+
+    Date dateCreated
+    Date lastUpdated
+
+    static constraints = {
+    }
+}
